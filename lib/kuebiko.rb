9e7b4214f2162110b2997b8eb5d8222f532bbae3
David Ramalho <dramalho@gmail.com>
2014-06-22 19:29:26 +0100

    Added Document payload type

--- lib/kuebiko.rb
+++ lib/kuebiko.rb
@@ -7,6 +7,7 @@ require 'kuebiko/message_payload.rb'
 
 require 'kuebiko/message_payload/generic'
 require 'kuebiko/message_payload/query'
+require 'kuebiko/message_payload/document'
 require 'kuebiko/message_payload/system_command'
 require 'kuebiko/message_payload/system_command/ping'
 require 'kuebiko/message_payload/system_command/shutdown'
